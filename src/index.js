d62f3784d9514d093636ad59b152fd4f69835f85
Ayushi Jain <ayushi.jain@globalnodes.ai>
2025-09-02 10:54:32 +0530

    utils code.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at the port : ${process.env.PORT}`);
+    });
+  })
+
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
